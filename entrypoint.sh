70b9e9136f64d9a4f7c842599c0564d301ee7ad2
GI-KACE-DEV <dev.aiti.com.gh@gmail.com>
2024-08-13 16:16:16 +0000

    wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,31 @@
-#!/bin/sh
-composer install
+#!/bin/bash
 
-npm install
+if [ ! -f "vendor/autoload.php" ]; then
+    composer install --no-progress --no-interaction
+fi
 
-npm run dev
-# Run database migrations
-php artisan migrate:fresh
+if [ ! -f ".env" ]; then
+    echo "Creating env file for env $APP_ENV"
+    cp .env.example .env
+else
+    echo "env file exists."
+fi
 
-#php artisan db:seed
-php artisan serve
+role=${CONTAINER_ROLE:-app}
+
+if [ "$role" = "app" ]; then
+    php artisan migrate:fresh
+    php artisan db:seed
+    php artisan key:generate
+    php artisan cache:clear
+    php artisan config:clear
+    php artisan route:clear
+    php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+    exec docker-php-entrypoint "$@"
+elif [ "$role" = "queue" ]; then
+    echo "Running the queue ... "
+    php /var/www/artisan queue:work --verbose --tries=3 --timeout=180
+elif [ "$role" = "websocket" ]; then
+    echo "Running the websocket server ... "
+    php artisan websockets:serve
+fi
