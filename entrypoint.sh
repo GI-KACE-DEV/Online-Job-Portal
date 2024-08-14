0aa6bf0838bfb4b50a2a83fffb9cb0db00268ee8
GI-KACE-DEV <dev.aiti.com.gh@gmail.com>
2024-08-14 09:44:48 +0000

    umcomment target php

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ if [ "$role" = "app" ]; then
     php artisan cache:clear
     php artisan config:clear
     php artisan route:clear
-    php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
+    php artisan serve --port=10000 --host=0.0.0.0 --env=.env
     exec docker-php-entrypoint "$@"
 elif [ "$role" = "queue" ]; then
     echo "Running the queue ... "
